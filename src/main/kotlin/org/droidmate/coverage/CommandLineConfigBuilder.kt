4e4d04d3210599ed7d00b30420d26945978e4815
Chanh Duc Ngo <chanhduc@gmail.com>
2021-08-03 13:41:59 +0200

    Support custom config file

--- src/main/kotlin/org/droidmate/coverage/CommandLineConfigBuilder.kt
+++ src/main/kotlin/org/droidmate/coverage/CommandLineConfigBuilder.kt
@@ -5,10 +5,14 @@ import com.natpryce.konfig.Configuration
 import com.natpryce.konfig.ConfigurationProperties
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.parseArgs
+import org.droidmate.configuration.ConfigProperties
+import java.io.File
 
 object CommandLineConfigBuilder {
     fun build(args: Array<String>): Configuration = build(parseArgs(
         args,
+        CommandLineOption(ConfigProperties.Core.configPath,
+            description = "Path to a custom configuration file, which replaces the default configuration.", short = "config"),
         CommandLineOption(CommandLineConfig.apk,
             description = "Apk to be instrumented. If a directory is provided, take the first non-instrumented app",
             short = "apk",
@@ -32,7 +36,24 @@ object CommandLineConfigBuilder {
     ).first)
 
     private fun build(cfgCommandLine: Configuration): Configuration {
-        return cfgCommandLine overriding
-                ConfigurationProperties.fromResource("coverageCommandLineConfig.properties")
+        val defaultConfig = ConfigurationProperties.fromResource("coverageCommandLineConfig.properties")
+
+        val customFile = when {
+            cfgCommandLine.contains(ConfigProperties.Core.configPath) -> File(cfgCommandLine[ConfigProperties.Core.configPath].path)
+            defaultConfig.contains(ConfigProperties.Core.configPath) -> File(defaultConfig[ConfigProperties.Core.configPath].path)
+            else -> null
+        }
+
+        val config : Configuration =
+            // command line
+            cfgCommandLine overriding
+                    // overrides custom config file
+                    (if (customFile?.exists() == true)
+                        ConfigurationProperties.fromFile(customFile)
+                    else
+                        cfgCommandLine) overriding
+                    // overrides default config file
+                    defaultConfig
+        return config
     }
 }
